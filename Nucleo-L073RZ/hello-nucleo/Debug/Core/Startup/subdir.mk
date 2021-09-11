b8492fe9a7eb83ade8c5b1c56e3a44bff1b87d73
Carmine Noviello <carmine.noviello@gmail.com>
2021-09-12 00:00:37 +0200

    Added Nucleo-L152RE and Nucleo-L073RZ workspaces

---
+++ Nucleo-L073RZ/hello-nucleo/Debug/Core/Startup/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32l073rztx.s 
+
+OBJS += \
+./Core/Startup/startup_stm32l073rztx.o 
+
+S_DEPS += \
+./Core/Startup/startup_stm32l073rztx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+
