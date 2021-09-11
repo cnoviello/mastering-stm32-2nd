63bc00e62f428a8c5e270cac9c7efa5175b423a1
Carmine Noviello <carmine.noviello@gmail.com>
2021-09-11 23:51:49 +0200

    Added Nucleo-L476RG workspace

---
+++ Nucleo-L476RG/hello-nucleo/Debug/Core/Startup/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/Startup/startup_stm32l476rgtx.s 
+
+OBJS += \
+./Core/Startup/startup_stm32l476rgtx.o 
+
+S_DEPS += \
+./Core/Startup/startup_stm32l476rgtx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
